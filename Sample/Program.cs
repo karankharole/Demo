43db4142341b80c3f785c2e4efdcc8da9934f597
karankharole <karankharole2000@gmail.com>
2022-12-31 09:39:05 +0530

    [karan] adding two numbers

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            int a = 2;
+            int b = 3;
+            int add = a + b;
+            Console.WriteLine(add);
         }
     }
 }
